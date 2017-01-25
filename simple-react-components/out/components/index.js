7197fd4a35fea5a5c59a53f71fb6e1fc79d302fa
Craig Condon <craig.j.condon@gmail.com>
2017-01-25 13:56:07 -0500

    initial commit

--- simple-react-components/out/components/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./button"));
-__export(require("./modal"));
-__export(require("./popover"));
-//# sourceMappingURL=index.js.map
